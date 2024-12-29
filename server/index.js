54b9336b59a408c53b9921accedd88974193c71b
TejasJain03 <tejaskjain2003@gmail.com>
2024-12-29 14:55:54 +0530

    Final Changes

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: "https://attendance-client-woad.vercel.app",
   // origin: "http://localhost:5173",
+  
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
